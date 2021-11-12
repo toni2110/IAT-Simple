cde9980461c2cb4b1003a166dfbd13a4c6687115
toni2110 <92384247+toni2110@users.noreply.github.com>
2021-11-12 17:21:07 +0100

    Update IAT.js

--- IAT.js
+++ IAT.js
@@ -3,37 +3,37 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 
 	return iatExtension({
 		category1 : {
-			name : 'Black people', //Will appear in the data.
+			name : 'green flower', //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
+				media : {word : 'Green flower'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		  	{image: 'flower-green.jpg'},
+            		{image: 'flower-pink.jpg'},
+            		{image: 'flower-yellow.jpg'}, 
+    			{image: 'flower-green.jpg'},
+            		{image: 'flower-pink.jpg'},
+            		{image: 'flower-yellow.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'White people', //Will appear in the data.
+			name : 'white flower', //Will appear in the data.
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
+				media : {word : 'White flower'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		{image: 'flower-white.jpg'},
-                {image: 'flower-purple.jpg'},
-                {image: 'flower-red.jpg'},
-		{image: 'flower-green.jpg'},
-            	{image: 'flower-pink.jpg'},
-            	{image: 'flower-yellow.jpg'}			], 
+    			{image: 'flower-white.jpg'},
+                	{image: 'flower-purple.jpg'},
+                	{image: 'flower-red.jpg'},
+			{image: 'flower-white.jpg'},
+                	{image: 'flower-purple.jpg'},
+                	{image: 'flower-red.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
